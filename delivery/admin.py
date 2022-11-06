4e92f27c0f0f49adebb6974c78a621014a851627
Juliet <agutu@gmail.com>
2022-11-06 15:58:49 +0300

    register models in admin.py

--- delivery/admin.py
+++ delivery/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from.models import Vendor, Customer, Order, Profile
 
-# Register your models here.
+admin.site.register(Vendor)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Profile)
