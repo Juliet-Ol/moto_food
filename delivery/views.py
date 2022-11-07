975b1986f7c32450f17537579c9ae666a1e2a7a4
Juliet <agutu@gmail.com>
2022-11-07 17:01:54 +0300

    create views for landing page

--- delivery/views.py
+++ delivery/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpRequest
+from .models import Vendor, Customer, Order, Profile
+from django.contrib import messages
+
+from django.conf import settings
+from django.core.mail import send_mail
+
 
 # Create your views here.
 
-def home():
-    return home
+def index(request):
+    return render(request, 'moto/index.html', {'name': 'Juliet'}) 
+
+
+
+
+    
+
