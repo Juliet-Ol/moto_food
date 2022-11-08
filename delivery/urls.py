9dfe29edc9c722571712aae9f799510906b4d123
Juliet <agutu@gmail.com>
2022-11-08 15:00:06 +0300

    under url add path for one user signup form

--- delivery/urls.py
+++ delivery/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+# from django.contrib import admin
+
 from . import views
+from delivery import views
 
 urlpatterns = [
-    path('index/', views.index),
+    path('', views.index, name='index'),
+    path('accounts/register/', views.register, name='register'),
+    
     
     
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
