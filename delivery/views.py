c5ec2c324fe928027fea4a014c72d5edef853708
Juliet <agutu@gmail.com>
2022-11-08 15:02:06 +0300

    views.py: add signup view

--- delivery/views.py
+++ delivery/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpRequest
 from .models import Vendor, Customer, Order, Profile
 from django.contrib import messages
+from .forms import VendorForm, CustomerForm, OrderForm, ProfileForm
 
 from django.conf import settings
 from django.core.mail import send_mail
@@ -11,9 +12,37 @@ from django.core.mail import send_mail
 # Create your views here.
 
 def index(request):
-    return render(request, 'moto/index.html', {'name': 'Juliet'}) 
+    return render(request, 'moto/index.html') 
 
 
+def register(request):
+    form = UserCreationForm
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+   
+            profile = Customer()
+            profile.full_name = form.cleaned_data['username']
+            # profile.delivery = None
+            # profile.user = user
+            profile.save()
+
+            messages.success(request, 'User has been registered')
+
+            subject = 'welcome to Moto food'
+            message = f'Hi {user.username}, thank you for registering for Moto food'
+            email_from = settings.EMAIL_HOST_USER
+            recipient_list = [user.email, ]
+            send_mail( subject, message, email_from, recipient_list )
+
+            return redirect ('/accounts/login')
+        else:
+            return render(request, 'registration/signup.html', {'form': form})
+
+    else:
+        return render(request, 'registration/signup.html', {'form': form})    
 
 
     
