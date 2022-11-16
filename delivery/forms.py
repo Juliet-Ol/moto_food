a1f8f88e60561ab5a47f11af4dba8ccea96dfb24
Juliet <agutu@gmail.com>
2022-11-16 23:08:04 +0300

    Forms: create forms under forms.py file

--- delivery/forms.py
+++ delivery/forms.py
@@ -1,32 +1,155 @@
 from django import forms
-from . models import Vendor, Customer, Order, Profile 
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import User
+from delivery.models import User, Order, Profile 
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget= forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
 
 
-class VendorForm(forms.ModelForm):
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    email = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+
     class Meta:
-        model = Vendor
-        fields = '__all__'
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', 'is_admin', 'is_vendor', 'is_customer', 'is_rider')
 
 
-class CustomerForm(forms.ModelForm):
+class ProfileForm(forms.Form):
     class Meta:
-        model = Customer
-        fields = '__all__'
+        model = Profile
+        fields = ('username', 'email', 'full_name', 'photo', 'location', 'mobile_number')
+
+class EditProfileForm(forms.Form):
+    class Meta:
+        model = Profile
+        fields = ('username', 'email', 'full_name', 'photo', 'location', 'mobile_number')  
+
+        
 
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = '__all__'        
 
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = '__all__'
 
-class SignupForm(UserCreationForm):  
-    email = forms.EmailField(max_length=200, help_text='Required')  
-    class Meta:  
-        model = User  
-        fields = ('username', 'email', 'password1', 'password2')
+
+
+
+
+
+
+# from django import forms
+# from delivery.models import User, Order, Profile 
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.models import User
+# from django.db import transaction
+
+# class CustomerSignUpForm(UserCreationForm):
+#     interests = forms.ModelMultipleChoiceField(
+#         queryset=Order.objects.all().values_list(),
+#         widget=forms.CheckboxSelectMultiple,
+#         required=True
+#     )
+
+#     class Meta(UserCreationForm.Meta):
+#         model = User
+
+#     @transaction.atomic
+#     def save(self):
+#         user = super().save(commit=False)
+#         user.is_customer = True
+#         user.save()
+#         # customer = Customer.objects.create(user=user)
+#         # customer.order.add(*self.cleaned_data.get('order'))
+#         return user
+
+
+# class VendorSignUpForm(UserCreationForm):
+#     class Meta(UserCreationForm.Meta):
+#         model = User
+
+#     def save(self, commit=True):
+#         user = super().save(commit=False)
+#         user.is_vendor = True
+#         if commit:
+#             user.save()
+#         return user
+
+# class RiderSignUpForm(UserCreationForm):
+#     class Meta(UserCreationForm.Meta):
+#         model = User
+
+#     def save(self, commit=True):
+#         user = super().save(commit=False)
+#         user.is_rider = True
+#         if commit:
+#             user.save()
+#         return user		
+
+# class OrderForm(forms.ModelForm):
+#     class Meta:
+#         model = Order
+#         fields = '__all__'
+
+# class ProfileForm(forms.Form):
+#     class Meta:
+#         model = Profile
+#         fields = ('username', 'email', 'password1', 'password2')
+
+# class EditProfileForm(forms.Form):
+#     class Meta:
+#         # model = Profile
+#         fields = '__all__'        
+
+# class SignupForm(UserCreationForm):  
+#     email = forms.EmailField(max_length=200, help_text='Required')  
+#     class Meta:  
+#         model = User  
+#         fields = ('username', 'email', 'password1', 'password2')
+
+# class PostForm(forms.Form):  
+#     email = forms.EmailField(max_length=200, help_text='Required')  
+#     class Meta:  
+#         model = User  
+#         fields = '__all__'     
