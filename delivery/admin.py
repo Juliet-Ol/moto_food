2d7bc5cf97d79bece95bef4a35e36db5f1fe2593
Juliet <agutu@gmail.com>
2022-11-16 23:08:49 +0300

    register models on admin

--- delivery/admin.py
+++ delivery/admin.py
@@ -1,7 +1,50 @@
 from django.contrib import admin
-from.models import Vendor, Customer, Order, Profile
+from .models import User
+from .models import Profile, Order
 
-admin.site.register(Vendor)
-admin.site.register(Customer)
-admin.site.register(Order)
+# Register your models here.
+admin.site.register(User)
 admin.site.register(Profile)
+admin.site.register(Order)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from django.contrib import admin
+# from.models import Vendor, Customer, Order, Profile, User
+# from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+# from django.contrib.auth.models import User
+
+# from django.contrib.auth.admin import UserAdmin
+# from .models import User
+
+# class SignupForm(UserCreationForm):
+#     class Meta:         
+#         model = User  
+#         fields = ('username', 'email', 'password1', 'password2')
+
+# class MyUserChangeForm(UserChangeForm):
+#     class Meta:
+#         model = User
+#         fields = ('username', 'email', )
+
+# class MyUserAdmin(UserAdmin):
+#     form = MyUserChangeForm
+#     add_form = SignupForm        
+        
+
+# admin.site.register(User, MyUserAdmin)
+# admin.site.register(Vendor)
+# admin.site.register(Customer)
+# admin.site.register(Order)
+# admin.site.register(Profile)
