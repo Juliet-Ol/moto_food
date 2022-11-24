1c90b099de010cc295c5f52a60c01745d117c5ab
Juliet <agutu@gmail.com>
2022-11-25 00:04:56 +0300

    register our models in admin.py

--- delivery/admin.py
+++ delivery/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from .models import User
-from .models import Profile, Order, Post
+# from .models import Profile, Order, Post
+from.models import *
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Profile)
 admin.site.register(Post)
+admin.site.register(Customer)
+admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
 
 
 
