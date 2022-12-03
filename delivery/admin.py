3b8cdc1b922e604f9694d3845edf58710f2f4c37
Juliet <agutu@gmail.com>
2022-12-04 01:59:03 +0300

    register cart and cartitem in admin site

--- delivery/admin.py
+++ delivery/admin.py
@@ -10,7 +10,8 @@ admin.site.register(Post)
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Order)
-admin.site.register(OrderItem)
+admin.site.register(CartItem)
+admin.site.register(Cart)
 admin.site.register(ShippingAddress)
 
 
