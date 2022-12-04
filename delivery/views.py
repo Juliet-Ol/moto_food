7126bee84e6c12c4e2baa6c5a852ae66206bd1ef
Juliet <agutu@gmail.com>
2022-12-04 23:16:53 +0300

    update cart and checkout on views and templates

--- delivery/views.py
+++ delivery/views.py
@@ -4,14 +4,11 @@ from django.shortcuts import HttpResponse, render, redirect
 from .forms import SignUpForm, LoginForm
 from django.contrib.auth import authenticate, login
 from .forms import ProfileForm, PostForm
-# from .models import Profile, Post
 from .models import *
 from django.contrib import messages
-
 from django.shortcuts import render, redirect
-
 from django.contrib.auth.decorators import login_required
-# from cart.cart import Cart
+
 
 
 def index(request):
@@ -39,16 +36,60 @@ def store (request):
     return render(request, 'cart/store.html', context)    
 
 
+def update_cart(request):
+    data = json.loads(request.body)
+    product_id = data["id"]
+    action = data["action"]
+      #added
+    product = Product.objects.get(id=product_id)
+
+
+    if request.user.is_authenticated:
+        cart = Cart.objects.get(user=request.user, completed=False)
+        
+        cartitem = CartItem.objects.get(cart=cart, product=product)
+
+
+        if action == 'inc':
+            cartitem.quantity += 1
+        else:
+            cartitem.quantity -= 1
+
+        if cartitem.quantity < 1:
+            cartitem.delete()
+        else:
+            cartitem.save()
+
+    return JsonResponse(cartitem.quantity, safe=False)
+
+
 def add_to_cart(request):
     data = json.loads(request.body)
     product_id = data["id"]
+      #added
     product = Product.objects.get(id=product_id)
 
 
     if request.user.is_authenticated:
         cart, created = Cart.objects.get_or_create(user=request.user, completed=False)
         cartitem, created = CartItem.objects.get_or_create(cart=cart, product=product)
+
+
         cartitem.quantity += 1
+
+    #added to try add and remove    
+        # if id == 'add': 
+        #     cartitem.quantity += 1   
+        
+            
+        
+
+        # elif id == 'remove':
+        #     cartitem.quantity -= 1 
+
+        # if cartitem.quantity <= 0:
+        #     cartitem.delete()       
+
         cartitem.save()
 
         num_of_items = cart.num_of_items
@@ -57,21 +98,30 @@ def add_to_cart(request):
     return JsonResponse(num_of_items, safe=False)
 
 
-def checkout(request):
-    # if request.method == 'POST':
-    # if request.user.is_autheticated:
-    # if request.user.customer:
-    #     customer = request.user.customer
-    #     order, created = Order.objects.get_or_create(customer=customer, complete=False)
-    #     items = order.orderitem_set.all()
-    #     cartItems = order.get_cart_items
-    # else:
-    #     items = []  
-    #     order ={'get_cart_total':0, 'get_cart_items':0,}
-    #     cartItems = order['get_cart_items']
 
-    context = {}
-    return render(request,'cart/checkout.html', context)       
+
+
+
+def checkout(request):
+    
+    if request.user.is_authenticated:
+        user = request.user
+        cart = Cart.objects.get(user=request.user, completed=False)
+        cartitems = cart.cartitems.all()
+
+    context = {'items': cartitems ,'cart':cart}
+    return render(request,'cart/checkout.html', context)   
+
+
+
+def post_checkout(request):
+    if request.user.is_authenticated:
+        cart = Cart.objects.get(user=request.user, completed=False)
+        cart.completed = True
+        cart.save()
+        messages.success(request, "Successfully made payments")
+
+    return redirect ('index')   
 
 
 
@@ -165,10 +215,24 @@ def approval(request):
             
             #update the database
             for x in id_list:
+                user = User.objects.filter(id=int(x))
+                print(user)
+                user.update(approved=True)
                 # User.objects.filter(pk=int(x)).update(approved=True) 
-                User.objects.filter().update(approved=True) 
-                Vendor.objects.filter().update(approved=True)
-                Rider.objects.filter().update(approved=True)
+
+                rider = Rider.objects.filter(full_name=user.first().full_name)
+                vendor = Vendor.objects.filter(full_name=user.first().full_name)
+
+                if rider is not None: {
+                    rider.update(approved=True)
+                }
+
+                if vendor is not None: {
+                    vendor.update(approved=True)
+                }
+                # User.objects.filter(pk=int(x)).update(approved=False) 
+                # Vendor.objects.filter(pk=int(x)).update(approved=False)
+                # Rider.objects.filter(pk=int(x)).update(approved=False)
             print(id_list)
 
             messages.success(request, 'User successfully approved')  
@@ -191,13 +255,112 @@ def approval(request):
     # return render(request, 'registration/approval.html')
 
 
+def admin(request):
+    return render(request,'profile/admin_profile.html')
 
 
+def customer(request):
+    return render(request,'profile/customer_profile.html')
 
 
+def vendor(request):
+    return render(request,'profile/vendor_profile.html')
+    
+
+def rider(request):
+    return render(request,'profile/rider_profile.html')   
 
 
+ 
 
+
+def edit_profile(request):
+
+    # profile= Profile.objects.all()
+
+    form = ProfileForm()
+
+   
+    return render(request, 'profile/edit_profile.html', {'form': form})      
+
+
+def profile(request):
+    form = ProfileForm(request.POST, request.FILES)
+    # profile=Profile.objects.get(user= request.user.id)
+    profile = Profile.objects.all()
+    # profile=Profile.objects.get()
+    print(profile)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST)
+       
+       
+        if form.is_valid():
+            print(profile)
+            image=form.cleaned_data['image']  
+            full_name=form.cleaned_data['full_name']  
+            email=form.cleaned_data['email'] 
+            # username=form.cleaned_data['username']
+            profile = {'image':image, 'full_name':full_name, 'email':email}
+            print(profile)
+           
+            # profile.username=request.user     
+
+              
+            form.save()            
+
+            # profile=Profile.objects.get(author= request.user.id)
+            # messages.success(request, 'Profile has been updated')
+
+            return redirect ('/profile')
+        else:
+            return render(request, 'profile/edit_profile.html', {'form': form})
+
+    # else:
+        
+
+    #     if Profile.objects.filter(user = request.user).count()
+    #         profile = Profile(user=request.user, full_name=request.user.full_name, email=request.user.email)
+    #         profile.save()
+    else:
+        # profile= request.user.profile 
+        return render(request, 'profile/profile.html', {'form': form, 'profile':profile})      
+
+
+def post(request):
+    form = PostForm()
+    current_user = request.user
+    if request.method == 'POST':
+        # print(request.POST['post'])
+        form = PostForm(request.POST, request.FILES)
+        
+        if form.is_valid():
+            post = Product()
+            name = form.cleaned_data['name']
+            price = form.cleaned_data['price']
+            author = current_user            
+            image = form.cleaned_data['image']
+            description = form.cleaned_data['description']
+            post = {'image':image, 'name':name, 'price':price, 'description': description  }
+            form.save()
+            messages.success(request, 'Saved product')
+            print (post)
+
+            return redirect ('/store')
+        else:
+            return render(request, 'food_vendor/new_post.html', {'form': form})
+
+    else:
+        return render(request, 'food_vendor/new_post.html', {'form': form})  
+
+
+def show_post(request):  
+
+    return render(request, 'food_vendor/show_post.html',)         
+
+
+def new_post(request):  
+    form = PostForm()
+    return render(request, 'food_vendor/new_post.html', {'form' : form})   
 
 
 
@@ -311,250 +474,25 @@ def approval(request):
 
 
 
-# def approval(request):
+#
 
-#     user_list = User.objects.all()
-#     print(user_list)
-#     if request.user.is_superuser:
-#         if request.method == "POST":
-#             id_list = request.POST.getlist('boxes')
-#             print(id_list)
 
-            
 
-#             messages.success(request, 'User successfully approved')  
-#             return redirect('index') 
 
-        
 
-#         else:   
-#             return render (request,'registration/approval.html',{'user_list': user_list})
 
-#     else:
-#         messages.success(request, 'you are not authorised to view this page')  
 
-#         return redirect('index')  
 
 
-        # return render (request,'registration/approval.html')    
 
 
 
 
-# @login_required(login_url="/users/login")
-# def cart_add(request, id):
-#     user = request.user
-#     cart = Cart(request)
-#     product = Product.objects.get(id=id)
-#     order, created = Order.objects.get_or_create(id=id, complete=False)
 
-#     orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
-#     cart.add(product=product)
-#     order.save()
-#     orderItem.save()
-#     print (cart)
-#     print (product)
-#     print (orderItem)
-#     print(order)  
-#     print(user)
-    
-       
-#     return redirect("cart_detail")
 
 
-# @login_required(login_url="/users/login")
-# def item_clear(request, id):
-#     cart = Cart(request)
-#     product = Product.objects.get(id=id)
-#     cart.remove(product)
-#     return redirect("cart_detail")
 
 
-# @login_required(login_url="/users/login")
-# def item_increment(request, id):
-#     cart = Cart(request)
-#     product = Product.objects.get(id=id)
-#     cart.add(product=product)
-#     return redirect("cart_detail")
-
-
-# @login_required(login_url="/users/login")
-# def item_decrement(request, id):
-#     cart = Cart(request)
-#     product = Product.objects.get(id=id)
-#     cart.decrement(product=product)
-#     if product is None:
-#         cart.remove(product)
-#     return redirect("cart_detail")
-
-
-# @login_required(login_url="/users/login")
-# def cart_clear(request):
-#     cart = Cart(request)
-#     cart.clear()
-#     return redirect("cart_detail")
-
-
-# @login_required(login_url="/users/login")
-# def cart_detail(request):
-#     return render(request, 'cart/cart_detail.html')
-
-
-
-
-
-
-# def logout_view(request):
-#     return render(request, 'moto/index.html')  
-# 
-# 
-
-  
-
-#   
-
-
-def admin(request):
-    return render(request,'profile/admin_profile.html')
-
-
-def customer(request):
-    return render(request,'profile/customer_profile.html')
-
-
-def vendor(request):
-    return render(request,'profile/vendor_profile.html')
-    
-
-def rider(request):
-    return render(request,'profile/rider_profile.html')   
-
-
- 
-
-
-def edit_profile(request):
-
-    # profile= Profile.objects.all()
-
-    form = ProfileForm()
-
-   
-    return render(request, 'profile/edit_profile.html', {'form': form})      
-
-
-def profile(request):
-    form = ProfileForm(request.POST, request.FILES)
-    # profile=Profile.objects.get(user= request.user.id)
-    profile = Profile.objects.all()
-    # profile=Profile.objects.get()
-    print(profile)
-    if request.method == 'POST':
-        form = ProfileForm(request.POST)
-       
-       
-        if form.is_valid():
-            print(profile)
-            image=form.cleaned_data['image']  
-            full_name=form.cleaned_data['full_name']  
-            email=form.cleaned_data['email'] 
-            # username=form.cleaned_data['username']
-            profile = {'image':image, 'full_name':full_name, 'email':email}
-            print(profile)
-           
-            # profile.username=request.user     
-
-              
-            form.save()            
-
-            # profile=Profile.objects.get(author= request.user.id)
-            # messages.success(request, 'Profile has been updated')
-
-            return redirect ('/profile')
-        else:
-            return render(request, 'profile/edit_profile.html', {'form': form})
-
-    # else:
-        
-
-    #     if Profile.objects.filter(user = request.user).count()
-    #         profile = Profile(user=request.user, full_name=request.user.full_name, email=request.user.email)
-    #         profile.save()
-    else:
-        # profile= request.user.profile 
-        return render(request, 'profile/profile.html', {'form': form, 'profile':profile})      
-
-
-def post(request):
-    form = PostForm(request.POST, request.FILES)
-    post = Post.objects.all()
-    current_user = request.user
-    if request.method == 'POST':
-        print(request.POST['post'])
-        form = PostForm(request.POST, request.FILES)
-        
-        if form.is_valid():
-            # post = Post()
-            post = form.save()
-            title = form.cleaned_data['title']
-            post = form.cleaned_data['post']
-            author = current_user            
-            picture = form.cleaned_data['picture']
-            price = form.cleaned_data['price']
-            post = {'picture':picture, 'author':author, 'title':title, 'price':price }
-            form.save()
-            messages.success(request, 'Posted')
-            print (post)
-
-        # posts = Post.objects.all
-        # context = {'posts': posts}
-
-        return redirect ('store')
-    else:
-        return render(request, 'food_vendor/new_post.html', {'form': form})
-
-        # else:
-        # return render(request, 'food_vendor/new_post.html', {'form': form})  
-
-
-def show_post(request):  
-
-    return render(request, 'food_vendor/show_post.html',)      
-
-
-
-
-
-
-
-
-
-# def post(request):
-#     form = PostForm(request.POST, request.FILES)
-#     post = Post.objects.all()
-#     current_user = request.user
-#     if request.method == 'POST':
-#         print(request.POST['post'])
-#         form = PostForm(request.POST, request.FILES)
-        
-#         if form.is_valid():
-#             # post = Post()
-#             title = form.cleaned_data['title']
-#             post = form.cleaned_data['post']
-#             author = current_user            
-#             picture = form.cleaned_data['picture']
-#             price = form.cleaned_data['price']
-#             post = {'picture':picture, 'author':author, 'title':title, 'price':price }
-#             form.save()
-#             messages.success(request, 'Posted')
-#             print (post)
-
-#             return redirect ('/show_post')
-#         else:
-#             return render(request, 'food_vendor/new_post.html', {'form': form})
-
-#     else:
-#         return render(request, 'food_vendor/new_post.html', {'form': form})  
 
 
 
