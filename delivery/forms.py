a5d965fa294f1464cdf99aaac87fe8a9f7d1b330
Juliet <agutu@gmail.com>
2022-12-09 00:11:06 +0300

    change profile form

--- delivery/forms.py
+++ delivery/forms.py
@@ -64,53 +64,53 @@ class SignUpForm(UserCreationForm):
         fields = ('username', 'email', 'full_name', 'password1', 'password2', 'is_admin', 'is_vendor', 'is_customer', 'is_rider')
 
 
-class ProfileForm(forms.ModelForm):
-    username = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                "class": "form-control"
-            }
-        )
-    )
-    # photo = forms.clo(
-    #     widget=forms.PasswordInput(
-    #         attrs={
-    #             "class": "form-control"
-    #         }
-    #     )
-    # )
-    mobile_number = forms.IntegerField(
-        widget=forms.NumberInput(
-            attrs={
-                "class": "form-control"
-            }
-        )
-    )
-    email = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                "class": "form-control"
-            }
-        )
-    )
+# class ProfileForm(forms.ModelForm):
+#     username = forms.CharField(
+#         widget=forms.TextInput(
+#             attrs={
+#                 "class": "form-control"
+#             }
+#         )
+#     )
+#     # photo = forms.clo(
+#     #     widget=forms.PasswordInput(
+#     #         attrs={
+#     #             "class": "form-control"
+#     #         }
+#     #     )
+#     # )
+#     mobile_number = forms.IntegerField(
+#         widget=forms.NumberInput(
+#             attrs={
+#                 "class": "form-control"
+#             }
+#         )
+#     )
+#     email = forms.CharField(
+#         widget=forms.TextInput(
+#             attrs={
+#                 "class": "form-control"
+#             }
+#         )
+#     )
 
-    full_name = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                "class": "form-control"
-            }
-        )
-    )
+#     full_name = forms.CharField(
+#         widget=forms.TextInput(
+#             attrs={
+#                 "class": "form-control"
+#             }
+#         )
+#     )
 
-    class Meta:
-        model = Profile
-        fields = ('username', 'email', 'photo', 'location', 'full_name', 'mobile_number')
-
-# class ProfileForm(forms.ModelForm):    
 #     class Meta:
 #         model = Profile
-#         fields = ( 'email', 'photo', 'location', 'full_name', 'mobile_number')
-#         fields = '__all__'
+#         fields = ('username', 'email', 'photo', 'location', 'full_name', 'mobile_number')
+
+class ProfileForm(forms.ModelForm):    
+    class Meta:
+        model = Profile
+        fields = ( 'email', 'photo', 'location', 'full_name', 'mobile_number')
+        fields = '__all__'
 
 class PostForm(forms.ModelForm):
     class Meta:
